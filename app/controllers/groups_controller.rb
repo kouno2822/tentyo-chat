abe1f40edf1844ec3e43bb71c1818b20f66b8243
kouno2822 <63778345+kouno2822@users.noreply.github.com>
2020-06-19 18:27:09 +0900

    finish-group&user.relation_refactoring

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,24 +2,39 @@ class GroupsController < ApplicationController
   def new
     @group = Group.new
   end
-
+  
   def create
     @group = Group.new(group_params)
+    @group.users << current_user
     if @group.save
       redirect_to root_path, notice: 'グループを作成しました'
     else
       render :new
     end
   end
-
+  
   def edit
+  end
+  
+  def update
+    if @group.update
+      ridirect_to messages_path
+    else
+      render root_path
+    end
   end
 
   def index
   end
 
+  def join
+    @group = Group.find(:id)
+    @group.users << current_user
+    @group.save
+  end
+
   private
   def group_params
-    params.require(:group).permit(:group_name, user_ids: [])
+    params.require(:group).permit(:group_name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,11 @@ Rails.application.routes.draw do
     get "sign_out", :to => "users/sessions#destroy" 
   end
   root 'groups#index'
-  resources :messages
-  resources :groups
+  resources :groups do
+    member do
+      get :join
+    end
+    resources :messages
+  end
+    
 end
