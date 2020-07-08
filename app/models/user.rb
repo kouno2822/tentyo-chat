f02bbc50a824734043a24fe013de6551fa4d99f3
kouno2822 <63778345+kouno2822@users.noreply.github.com>
2020-07-08 23:37:31 +0900

    edit-user_edit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable
+         :recoverable, :rememberable, :validatable
   has_many :group_users
   has_many :messages
   has_many :groups, through: :group_users
   validates :name, presence: true, uniqueness: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email,  presence: true
+  validates :email,  uniqueness: { case_sensitive: true }, format: { with: VALID_EMAIL_REGEX }, allow_blank: true
 end
