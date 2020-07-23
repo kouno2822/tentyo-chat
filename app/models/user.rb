322ee221f9942678d93bd43c55fdce2f8ca78973
kouno2822 <63778345+kouno2822@users.noreply.github.com>
2020-07-23 17:55:52 +0900

    user-edit-fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,28 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable
   has_many :group_users
   has_many :messages
   has_many :groups, through: :group_users
+
   validates :name, presence: true, uniqueness: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email,  presence: true
   validates :email,  uniqueness: { case_sensitive: true }, format: { with: VALID_EMAIL_REGEX }, allow_blank: true
+  validates :password, presence: true
+  validates :password, confirmation: true, length: { minimum: 6 }, allow_blank: true
+  validates :password_confirmation,presence: true
+
+  def update_without_current_password(params, *options)
+    params.delete(:current_password)
+ 
+    if params[:password].blank? && params[:password_confirmation].blank?
+      params.delete(:password)
+      params.delete(:password_confirmation)
+    end
+ 
+    result = update_attributes(params, *options)
+    clean_up_passwords
+    result
+  end
 end
