1ee6ec48831f1b0a1d86d693f6c7bf5c31fdcd74
kouno2822 <63778345+kouno2822@users.noreply.github.com>
2020-08-28 16:34:24 +0900

    signout&signin reset

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   devise_scope :user do
     get "sign_in", to:  "users/sessions#new"
     get "sign_out", to: "users/sessions#destroy" 
-    get 'users/guest_sign_in', to: 'users/sessions#new_guest'
+    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
   end
   root 'groups#index'
   resources :groups do
